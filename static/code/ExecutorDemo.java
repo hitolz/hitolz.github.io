3882853ecd6b01559809fe1d451b57ad350d5624
朱海涛 <i@hitol.me>
2019-12-26 20:52:39 +0800

    多线程相关

--- static/code/ExecutorDemo.java
+++ static/code/ExecutorDemo.java
@@ -1,7 +1,12 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ExecutorDemo {
     public static void main(String[] args) {
@@ -12,7 +17,37 @@ public class ExecutorDemo {
         // 创建单线程的线程池
         ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
         // 创建定时执行的线程池
-        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool();
+        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(10);
+
+        // newCachedThreadPool 源码
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+                60L, TimeUnit.SECONDS,
+                new SynchronousQueue<Runnable>());
+
+        // newFixedThreadPool 源码
+        ThreadPoolExecutor threadPoolExecutor1 = new ThreadPoolExecutor(5, 5,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+
+
+        // newSingleThreadExecutor源码
+        // 封装成FinalizableDelegatedExecutorService
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(1, 1,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+
+        // newScheduledThreadPool 源码
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(10);
+
+
+        Future<?> future = cachedThreadPool.submit(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
+        future.get();
 
     }
 }
